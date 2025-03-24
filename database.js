89dd4b5c25bb33ff68a88b179ec3a82f68d614db
nhanlekha <nhanlekha@gmail.com>
2025-03-24 14:42:29 +0700

    update env

--- database.js
+++ database.js
@@ -3,11 +3,11 @@ const mysql = require("mysql2");
 
 // Tạo pool kết nối cho MySQL
 const pool = mysql.createPool({
-  host: "localhost",          // hoặc host của MySQL server
-  user: "root",       // thay bằng tên user của bạn
-  password: "",   // thay bằng mật khẩu
-  database: "testdatabase",   // tên database
-  port: 3306,                 // cổng mặc định của MySQL
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USERNAME, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,  
+  port:  process.env.PORT,                 // cổng mặc định của MySQL
   connectionLimit: 10
 });
 
